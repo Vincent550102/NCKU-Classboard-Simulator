2ef908fb78f7ee6aa761c39bcbfbb4417ea6377e
Vincent550102 <Vincent550102@users.noreply.github.com>
2022-07-12 09:01:59 +0000

    deploy: 02494474c1f0543832a6f12dd5f8388e894e7677

--- index.js
+++ index.js
@@ -90,6 +90,20 @@ function addBtnModal(classId, classEle) {
     })
 }
 
+function renderCredits() {
+    $.getJSON("111data.json", function (json) {
+        let currentCredits = 0
+        if (Cookies.get('classSess') === undefined)
+            return
+        var classSess = JSON.parse(atob(Cookies.get('classSess')))
+        classSess.forEach(element => {
+            console.log(element);
+            currentCredits += parseInt(json[element].credit)
+        })
+        $("#credits").text(currentCredits);
+    });
+}
+
 function renderClassbtn(classId, classEle, styleBagIndex) {
     var btnstyle = pickbtnstyle(styleBagIndex)
     classEle["time"].forEach(element => {
@@ -256,6 +270,7 @@ function copyToClipboard(copy_text) {
 
 
 function updExportmess() {
+    renderCredits();
     $("#urlButton_code").html(`<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clipboard" viewBox="0 0 16 16">
     <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"/>
     <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"/>
@@ -279,7 +294,6 @@ function updExportmess() {
         $("#export_input_url").attr("value", `${window.location.href.split('?')[0]}?share=${encodeURIComponent(Cookies.get('classSess'))}`)
         $("#urlButton_url").attr("urlAttr", `${window.location.href.split('?')[0]}?share=${encodeURIComponent(Cookies.get('classSess'))}`)
     }
-
 }
 
 function putExportCopyBtn() {
